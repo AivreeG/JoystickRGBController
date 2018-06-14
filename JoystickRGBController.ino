c9315223cf5b7629ad9172d37aefbf78219c2cca
AivreeG <aivreeapps@gmail.com>
2018-06-14 01:57:21 -0500

    Performance improvements by removing unnecessary computations

--- JoystickRGBController.ino
+++ JoystickRGBController.ino
@@ -1,45 +1,43 @@
+/*JoystickRGBController.ino
+ * Created by Aivree Gomez 
+ * Used to change the color of an RGB LED using 
+ * an analog joystick.
+ */
 #include <math.h>
-#define BLUE_PIN 6;
+#define BLUE_PIN 6
 #define GREEN_PIN 5
 #define RED_PIN 3
-#define yPin A1
-#define xPin A0
+#define X_PIN A0
+#define Y_PIN A1
 const double GREEN_MAX_VAL = 255;
-const double RED_MAX_VAL = 255 * .5;
+const double RED_MAX_VAL = 255;
 const double BLUE_MAX_VAL = 255;
+
 double yVal;
 double xVal;
-double xIn;
-double yIn;
 double theta;
 double pwm;
 double deg;
-double r;
 
-// the setup routine runs once when you press reset:
 void setup() {
   pinMode(BLUE_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(RED_PIN, OUTPUT);
-
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  
-  yIn = analogRead(yPin);
-  xIn = analogRead(xPin);
-  xVal = map(xIn, 0, 1023, -127, 127);
-  yVal = map(yIn, 0, 1023, -127, 127);
+
+  xVal = map(analogRead(X_PIN), 0, 1023, -127, 127);
+  yVal = map(analogRead(Y_PIN), 0, 1023, -127, 127);
+
   theta = fabs(atan(yVal / xVal));
   deg = theta * 180 / M_PI;
-  r = sqrt(fabs(xVal * xVal) + fabs(yVal * yVal));
   
   if (xVal < 0 && yVal >= 0) deg = 90 - deg + 90;
   else if (xVal <= 0 && yVal < 0) deg += 180;
   else if (xVal >= 0 && yVal < 0) deg = (90 - deg) + 270;
   
-  if(r <= 5){
+  if(fabs(xVal) <= 5 && fabs(yVal) <= 5){
     analogWrite(BLUE_PIN, BLUE_MAX_VAL);
     analogWrite(GREEN_PIN, GREEN_MAX_VAL);
     analogWrite(RED_PIN, RED_MAX_VAL);
